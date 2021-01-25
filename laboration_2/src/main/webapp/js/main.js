6c0695030156ef0daa0d2df55f920a3d00d61f15
guspihl <g.pihlquist@gmail.com>
2021-01-25 20:28:48 +0100

    simple jquery for demo

--- laboration_2/src/main/webapp/js/main.js
+++ laboration_2/src/main/webapp/js/main.js
@@ -3,7 +3,6 @@ function search() {
     //document.getElementById("search_result").innerHTML = "You searched for keyword: " + document.getElementById("search_form").value;
     //document.getElementById("search_form").value = "";
     var search_result = $('#search_result');
-    var search_form = $('search_form').val();
-    //console.log(search_result.innerHTML);
-    console.log(search_form);
+    search_request = $('input[type="search"]').val();
+    search_result.html("You searched for keyword: " + search_request)
 }
