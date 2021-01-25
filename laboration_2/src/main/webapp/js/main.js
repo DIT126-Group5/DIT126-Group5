6ff402d775bbc90f56bd15481cce6faafb6c7c14
guspihl <g.pihlquist@gmail.com>
2021-01-25 16:25:20 +0100

    easy js quirk and refactoring

---
+++ laboration_2/src/main/webapp/js/main.js
@@ -0,0 +1,9 @@
+
+function search() {
+    //document.getElementById("search_result").innerHTML = "You searched for keyword: " + document.getElementById("search_form").value;
+    //document.getElementById("search_form").value = "";
+    var search_result = $('#search_result');
+    var search_form = $('search_form').val();
+    //console.log(search_result.innerHTML);
+    console.log(search_form);
+}
