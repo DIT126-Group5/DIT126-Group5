c3faafffac16e9e5bac626a0e05e582b23bc64f6
Benjamin Sannholm <benjamin@sannholm.se>
2021-02-09 12:02:57 +0100

    Finish ConditionDAO

--- BooksOfBliss/src/main/java/com/group05/booksofbliss/model/dao/ConditionDAO.java
+++ BooksOfBliss/src/main/java/com/group05/booksofbliss/model/dao/ConditionDAO.java
@@ -1,7 +1,16 @@
 package com.group05.booksofbliss.model.dao;
 
 import com.group05.booksofbliss.model.entity.Condition;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import lombok.Getter;
 
 public class ConditionDAO extends AbstractDAO<Condition, String> {
-    
+    @Getter
+    @PersistenceContext(unitName = "bobDB")
+    private EntityManager entityManager;
+
+    public ConditionDAO() {
+        super(Condition.class);
+    }
 }
