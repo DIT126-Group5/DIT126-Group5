14cf4db624c3647c8ffb2b43caa3538c7a7a095a
guspihl <g.pihlquist@gmail.com>
2021-02-09 11:46:31 +0100

    Condition entity

---
+++ BooksOfBliss/src/main/java/com/group05/booksofbliss/model/entity/Condition.java
@@ -0,0 +1,23 @@
+package com.group05.booksofbliss.model.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Condition implements Serializable {
+    @Id private String name;
+    
+    @OneToMany(mappedBy = "condition") private List<Listing> listings;
+
+    public Condition(String name) {
+        this.name = name;
+    }
+}
