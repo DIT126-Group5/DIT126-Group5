9a2cd562533b791bb32293579e8351aa9bd557de
Benjamin Sannholm <benjamin@sannholm.se>
2021-02-19 01:01:36 +0100

    Initial WIP checkout page with basic backing bean

--- BooksOfBliss/src/main/java/com/group05/booksofbliss/model/dao/ListingDAO.java
+++ BooksOfBliss/src/main/java/com/group05/booksofbliss/model/dao/ListingDAO.java
@@ -1,7 +1,6 @@
 package com.group05.booksofbliss.model.dao;
 
 import com.group05.booksofbliss.model.entity.Listing;
-import com.group05.booksofbliss.model.entity.QListing;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
@@ -10,29 +9,30 @@ import javax.persistence.PersistenceContext;
 import lombok.Getter;
 
 @Stateless
-public class ListingDAO extends AbstractDAO<Listing, String> {
+public class ListingDAO extends AbstractDAO<Listing, Integer> {
+
     @Getter
     @PersistenceContext(unitName = "bobDB")
     private EntityManager entityManager;
-    
+
     public ListingDAO() {
         super(Listing.class);
     }
-    
+
     //Not the best search method
-    public List<Listing> search(String searchInput){
+    public List<Listing> search(String searchInput) {
         List<Listing> listings = findAll();
         List<Listing> searchedListings = new ArrayList<>();
-        for(Listing listing : listings){
-            if(listing.getBook().getTitle().contains(searchInput) || 
-                    listing.getBook().getIsbn().contains(searchInput))
+        for (Listing listing : listings) {
+            if (listing.getBook().getTitle().contains(searchInput)
+                    || listing.getBook().getIsbn().contains(searchInput)) {
                 searchedListings.add(listing);
+            }
         }
         return searchedListings;
     }
+
     public List<Listing> sortListingsByDate(String order) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
-
-
