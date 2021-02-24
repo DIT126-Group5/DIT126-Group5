29d6b2e15402ed5e642bbb9d72456f3c1bd06421
guspihl <g.pihlquist@gmail.com>
2021-02-24 09:56:25 +0100

    continued work on publish page

--- BooksOfBliss/src/main/java/com/group05/booksofbliss/controller/PublishListingController.java
+++ BooksOfBliss/src/main/java/com/group05/booksofbliss/controller/PublishListingController.java
@@ -1,5 +1,6 @@
 package com.group05.booksofbliss.controller;
 
+import com.group05.booksofbliss.model.entity.Condition;
 import com.group05.booksofbliss.view.PublishListingBackingBean;
 import java.io.Serializable;
 import javax.faces.view.ViewScoped;
@@ -16,8 +17,13 @@ public class PublishListingController implements Serializable {
     //anropar create i ListingDAO.
     @Inject
     private PublishListingBackingBean publishListingBackingBean;
+    //private double price;
+    //private String description;
+    //private Condition condition;
     
-    public void publish(){
-        System.out.println("hej");
+    public void publish(double price, String description, Condition condition){
+        publishListingBackingBean.setPrice(price);
+        publishListingBackingBean.setDescription(description);
+        publishListingBackingBean.setCondition(condition);
     }
 }
--- BooksOfBliss/src/main/java/com/group05/booksofbliss/view/PublishListingBackingBean.java
+++ BooksOfBliss/src/main/java/com/group05/booksofbliss/view/PublishListingBackingBean.java
@@ -1,22 +1,36 @@
 package com.group05.booksofbliss.view;
 
+import com.group05.booksofbliss.model.dao.ConditionDAO;
 import com.group05.booksofbliss.model.dao.ListingDAO;
+import com.group05.booksofbliss.model.entity.Condition;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import lombok.Data;
 import lombok.Getter;
 
-
+@Data
 @Named
 @ViewScoped
-@Getter
 public class PublishListingBackingBean implements Serializable {
     
+    @Inject
     private ListingDAO listingDAO;
-    // Id, date, price, description, account, condition
+    @Inject
+    private ConditionDAO conditionDAO;
+    //List<Condition> conditions = conditionDAO.findAll();
+    private List<Condition> conditions;
+    private double price;
+    private String description;
+    private Condition condition;
+    
     @PostConstruct
-    public void init(){
-        //user = inloggadUser
+    public void init() {
+        conditions = new ArrayList();
+        conditions = conditionDAO.findAll();
     }
 }
