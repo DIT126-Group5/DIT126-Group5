fb39b88814b1da490a8d02cb6bdf6d3e392e3327
Sophia <sophia_pham@hotmail.com>
2021-03-09 22:05:26 +0100

    Empty file to show package structure of tests

--- BooksOfBliss/src/test/java/com/group05/booksofbliss/model/entity/AccountTest.java
+++ BooksOfBliss/src/test/java/com/group05/booksofbliss/model/entity/AccountTest.java
@@ -1,37 +1,4 @@
 
-import com.group05.booksofbliss.model.entity.Account;
-
-@RunWith(Arquillian.class)
 public class AccountTest {
 
-    @Deployment
-    public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class)
-                .addPackages(true, "com.group05.booksofbliss")
-                .addAsResource("META-INF/persistence.xml")
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
-
-    @Inject
-    private AccountDAO accountDAO;
-
-    private Account account = new Account("username", "firstName", "lastName", "+46 72 985 32 12", "email@hotmail.com", "Password123!", new Address("Sannegarden", "45242", "Göteborg"), Money.of(10, "SEK"));
-
-    @Before
-    public void init() {
-        accountDAO.create(account);
-    }
-
-    @After
-    public void clean() {
-        accountDAO.remove(account);
-    }
-
-    //Checks if the Username is correct.
-    @Test
-    public void checkThatFindUsersMatchingNameMatchesCorrectly() {
-        Assert.assertEquals(accountDAO
-                .findByUsername(account.getUsername()).getUsername(),
-                account.getUsername());
-    }
 }
