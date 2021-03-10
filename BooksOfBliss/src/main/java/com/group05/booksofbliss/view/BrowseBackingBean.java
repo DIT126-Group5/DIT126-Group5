0e45f7c85ca0637e6f2d024ece4ec4c73fb5ef3b
Benjamin Sannholm <benjamin@sannholm.se>
2021-03-10 18:38:38 +0100

    Delete unused classes and methods

--- BooksOfBliss/src/main/java/com/group05/booksofbliss/view/BrowseBackingBean.java
+++ BooksOfBliss/src/main/java/com/group05/booksofbliss/view/BrowseBackingBean.java
@@ -2,25 +2,14 @@ package com.group05.booksofbliss.view;
 
 import com.group05.booksofbliss.model.dao.BookDAO;
 import com.group05.booksofbliss.model.dao.ListingDAO;
-import com.group05.booksofbliss.model.entity.Account;
-import com.group05.booksofbliss.model.entity.Author;
-import com.group05.booksofbliss.model.entity.Book;
-import com.group05.booksofbliss.model.entity.Category;
-import com.group05.booksofbliss.model.entity.Condition;
 import com.group05.booksofbliss.model.entity.Listing;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
-import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import lombok.Data;
 import org.omnifaces.cdi.Param;
-import java.lang.StringBuilder;
 
 @Data
 @Named(value = "browseBackingBean")
@@ -42,19 +31,5 @@ public class BrowseBackingBean implements Serializable {
         } else {
             return listingDAO.search(searchQuery);
         }
-    }
-
-    public Listing getListing(Long id) {
-        return listingDAO.find(id);
-    }
-
-    public String getAuthorsAsString(String isbn) {
-        List<Author> authors = bookDAO.findByISBN(isbn).getAuthors();
-        StringBuilder sb = new StringBuilder();
-        authors.forEach(author -> {
-            sb.append(author.getName());
-            sb.append(", ");
-        });
-        return sb.toString();
     }
 }
