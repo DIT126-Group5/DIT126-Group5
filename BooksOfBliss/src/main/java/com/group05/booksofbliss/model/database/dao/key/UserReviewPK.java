b2ace137caf35b84781e46f4a31170e4fcca262e
Benjamin Sannholm <benjamin@sannholm.se>
2021-03-14 17:25:05 +0100

    Allow user to give a review after successful purchase

--- BooksOfBliss/src/main/java/com/group05/booksofbliss/model/database/dao/key/UserReviewPK.java
+++ BooksOfBliss/src/main/java/com/group05/booksofbliss/model/database/dao/key/UserReviewPK.java
@@ -1,9 +1,11 @@
 package com.group05.booksofbliss.model.database.dao.key;
 
 import java.io.Serializable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserReviewPK implements Serializable {
 
     private String reviewer;
---
+++ BooksOfBliss/src/main/java/com/group05/booksofbliss/model/service/ReviewService.java
@@ -0,0 +1,33 @@
+package com.group05.booksofbliss.model.service;
+
+import com.group05.booksofbliss.model.dao.UserReviewDAO;
+import com.group05.booksofbliss.model.database.dao.key.UserReviewPK;
+import com.group05.booksofbliss.model.entity.Account;
+import com.group05.booksofbliss.model.entity.Purchase;
+import com.group05.booksofbliss.model.entity.UserReview;
+import java.io.Serializable;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class ReviewService implements Serializable {
+
+    @Inject
+    private UserReviewDAO userReviewDAO;
+
+    @Transactional
+    public void reviewPurchase(Purchase purchase, String comment, int rating) {
+        Account reviewer = purchase.getAccount();
+        Account reviewee = purchase.getListing().getPublishedBy();
+
+        UserReview previousReview = userReviewDAO.find(new UserReviewPK(reviewer.getUsername(), reviewee.getUsername()));
+        if (previousReview != null) {  // If reviewer has previously reviewed reviewee, update with new comment and rating
+            previousReview.setRating(rating);
+            previousReview.setComment(previousReview.getComment() + '\n' + comment);
+            userReviewDAO.update(previousReview);
+        } else {
+            userReviewDAO.create(new UserReview(reviewer, reviewee, comment, rating));
+        }
+    }
+}
