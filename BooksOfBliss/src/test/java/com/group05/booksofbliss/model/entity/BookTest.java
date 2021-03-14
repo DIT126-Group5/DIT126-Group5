7081fd099acf3553be95828c5843e1498a0a63e9
Benjamin Sannholm <benjamin@sannholm.se>
2021-03-14 20:23:52 +0100

    Various tests for entity setter NPE

--- BooksOfBliss/src/test/java/com/group05/booksofbliss/model/entity/BookTest.java
+++ BooksOfBliss/src/test/java/com/group05/booksofbliss/model/entity/BookTest.java
@@ -3,12 +3,12 @@ package com.group05.booksofbliss.model.entity;
 import java.util.Set;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import org.junit.AfterClass;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class BookTest {
@@ -27,22 +27,43 @@ public class BookTest {
         validatorFactory.close();
     }
 
+    private Book book;
+
+    @Before
+    public void init() {
+        book = new Book("34124124124", "Calculus", 2007, "");
+    }
+
     @Test
     public void BlankTitleOrISBNShouldCauseViolation() {
         Book book = new Book("", "", 1996, "");
-        
+
         Set<ConstraintViolation<Book>> violations = validator.validate(book);
 
         assertTrue(violations.size() == 2);
     }
-    
-        @Test
+
+    @Test
     public void NonBlankTitleOrISBNShouldNotCauseViolation() {
         Book book = new Book("34124124124", "Title", 1996, "");
 
         Set<ConstraintViolation<Book>> violations = validator.validate(book);
-        
+
         assertTrue(violations.isEmpty());
     }
 
+    @Test(expected = NullPointerException.class)
+    public void setIsbn_givenNull_throwsNPE() {
+        book.setIsbn(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void setTitle_givenNull_throwsNPE() {
+        book.setTitle(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void setImage_givenNull_throwsNPE() {
+        book.setImageUrl(null);
+    }
 }
--- BooksOfBliss/src/test/java/com/group05/booksofbliss/model/entity/ConditionTest.java
+++ BooksOfBliss/src/test/java/com/group05/booksofbliss/model/entity/ConditionTest.java
@@ -3,12 +3,11 @@ package com.group05.booksofbliss.model.entity;
 import java.util.Set;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import org.junit.AfterClass;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ConditionTest {
@@ -30,19 +29,23 @@ public class ConditionTest {
     @Test
     public void BlankTitleOrISBNShouldCauseViolation() {
         Condition condition = new Condition("");
-        
+
         Set<ConstraintViolation<Condition>> violations = validator.validate(condition);
 
         assertTrue(violations.size() == 1);
     }
-    
-        @Test
+
+    @Test
     public void NonBlankTitleOrISBNShouldNotCauseViolation() {
         Condition condition = new Condition("Bra");
 
         Set<ConstraintViolation<Condition>> violations = validator.validate(condition);
-        
+
         assertTrue(violations.isEmpty());
     }
 
+    @Test(expected = NullPointerException.class)
+    public void setName_givenNull_throwsNPE() {
+        new Condition("Bra").setName(null);
+    }
 }
